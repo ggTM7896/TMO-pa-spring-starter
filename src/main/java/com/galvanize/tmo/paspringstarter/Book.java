515c69065ce02ad0019e0f326b40ab489e3617e4
Goslen <Gregory.Goslen1@T-Mobile.com>
2021-12-29 12:33:25 -0600

    Added Book.java and modified LibraryController.java to implement the back-end project requirements

---
+++ src/main/java/com/galvanize/tmo/paspringstarter/Book.java
@@ -0,0 +1,104 @@
+package com.galvanize.tmo.paspringstarter;
+import java.util.Objects;
+import org.json.JSONObject;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+class Book implements java.lang.Comparable<Book> {
+
+//  private @Id @GeneratedValue Long id;
+ 	private Long id;
+	private String title;
+	private String author;
+	private String yearPublished;
+
+	Book() {}
+
+	Book(String title, String author, String yearPublished) {
+		this.title = title;
+		this.author = author;
+		this.yearPublished = yearPublished;
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public String getAuthor() {
+		return this.author;
+	}
+
+	public String getYearPublished() {
+		return this.yearPublished;
+	}
+
+	public String getSortKey() {
+	    String key = this.title + " " + this.author + " " + this.yearPublished;
+	    return key;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public void setYearPublished(String yearPublished) {
+		this.yearPublished = yearPublished;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+
+		if (this == o)
+			return true;
+		if (!(o instanceof Book))
+			return false;
+
+		Book book = (Book) o;
+		return Objects.equals(this.id, book.id) &&
+		       Objects.equals(this.title, book.title) &&
+		       Objects.equals(this.author, book.author) &&
+		       Objects.equals(this.yearPublished, book.yearPublished);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.id, this.title, this.author, this.yearPublished);
+	}
+
+	@Override
+	public String toString() {
+		return "Book{id=" + this.id + ", title='" + this.title + "', author='" + this.author
+			+ "', yearPublished='" + this.yearPublished + "'" + "}";
+	}
+
+	public JSONObject toJSON() {
+		JSONObject entity = new JSONObject();
+		entity.put("author", this.author);
+		entity.put("id", this.id);
+		entity.put("title", this.title);
+		entity.put("yearPublished", this.yearPublished);
+		return(entity);
+	}
+	
+	@Override
+	public int compareTo(Book b2) {
+        return this.getSortKey().compareTo(b2.getSortKey());
+	}
+	
+}
+
+
